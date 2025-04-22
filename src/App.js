37f6183b4006f136cdef1d2b6ed548572610bb48
Marco Araya <markscr@gmail.com>
2025-04-21 18:15:52 -0700

    Changed page text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          Changed on feature1 branch
         </a>
       </header>
     </div>
